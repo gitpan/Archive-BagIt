bd76ebd34a2833d96cce5d95292711e23e6b7832
Robert Schmidt <rjeschmi@gmail.com>
2012-10-09 19:18:30 +0000

    Import of RJESCHMI/Archive-BagIt-0.02_1 from CPAN.
    gitpan-cpan-distribution: Archive-BagIt
    gitpan-cpan-version:      0.02_1
    gitpan-cpan-path:         RJESCHMI/Archive-BagIt-0.02_1.tar.gz
    gitpan-cpan-author:       RJESCHMI
    gitpan-cpan-maturity:     developer

---
+++ bin/bagit.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w 
+
+use strict;
+use Archive::BagIt;
+
+=head1 NAME
+
+bagit.pl - A commandline interface to the Perl bagit library
+
+=head1 SYNOPSIS
+
+  bagit.pl <directory>  - will create a bag in the specified directory
+
+=head1 DESCRIPTION 
+
+  Simple script that wraps up interesting parts of the Archive::BagIt interface
+
+
+=cut
+
+
+foreach my $filepath (@ARGV) {
+  my $bag = Archive::BagIt->make_bag($filepath);
+}
+
+
+
+
+1;
